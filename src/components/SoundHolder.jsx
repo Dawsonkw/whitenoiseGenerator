beee030ab678a00bc7cd7750a0a450802938b9c9
Dawsonkw <dawsonkw@gmail.com>
2023-04-13 10:31:14 -0600

    Repurposed idea / main goal of app to that of an ambient noise generator for tabletop RPG's like D&D, basic skeleton of front end, searching for an API to use for the sounds

---
+++ src/components/SoundHolder.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function SoundHolder(props) {
+    return (
+        <div>
+            <div className='icon-holder'> 
+                <div className='individualIconBox'>
+                    <p>Rain</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>City</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>Bonfire</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>Wind</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>Birds</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>Ocean</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>Cafe</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>Thunder</p>
+                </div>
+                <div className='individualIconBox'>
+                    <p>Static</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SoundHolder;
