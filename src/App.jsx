1d0e588df66f8467e7aa3c8041544abb43840195
Dawsonkw <dawsonkw@gmail.com>
2023-04-14 12:26:46 -0600

    Added in list of top level tags that will be used for searching for ambance, fixed API so that it displays correct info for the videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import ApiCaller from './api/ApiCaller'
-import SoundHolder from './components/SoundHolder'
+import Tags from './components/Tags'
 
 function App() {
+  
 
 
   return (
+    
     <div >
-      <ApiCaller />
+      
       <h1>Dungeons and Dragons Ambient Noise App</h1>
-      <SoundHolder />
+      <Tags />
+
+      <ApiCaller />
     </div>
   )
 }
---
+++ src/components/Tags.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Tags(props) {
+    return (
+        <div className='tagContainer'>
+            <ul className='tagHolder'>
+                <li>Battles</li>
+                <li>Traveling</li>
+                <li>Resting</li>
+                <li>Monsters</li>
+                <li>Exploration</li>
+                <li>Tension</li>
+                <li>Epic</li>
+                <li>Tavern</li>
+                <li>Magical</li>
+                <li>Stealth</li>
+                <li>Horror</li>
+                <li>Nostalgic</li>
+                <li>Suspense</li>
+                <li>Boss Battles</li>
+                <li>Introspective</li>
+                <li>Dreamlike</li>
+
+            </ul>
+        </div>
+    );
+}
+
+export default Tags;
